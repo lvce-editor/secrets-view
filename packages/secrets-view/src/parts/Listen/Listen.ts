3cb9ae8584d7673b97fd752892f2ba077c831ae1
Le Vivilet <levivilet@pm.me>
2026-08-27 08:39:10 -0700

    feature: connect secret storage to main process (#6)

--- packages/secrets-view/src/parts/Listen/Listen.ts
+++ packages/secrets-view/src/parts/Listen/Listen.ts
@@ -1,8 +1,9 @@
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import { initializeMainProcess } from '../InitializeMainProcess/InitializeMainProcess.ts'
 import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 import { registerCommands } from '../SecretsViewStates/SecretsViewStates.ts'
 
 export const listen = async (): Promise<void> => {
   registerCommands(CommandMap.commandMap)
-  await initializeRendererWorker()
+  await Promise.all([initializeRendererWorker(), initializeMainProcess()])
 }
